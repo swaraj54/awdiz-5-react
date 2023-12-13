ef117a175c6687db79140c896e292a9ea235d518
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-12-13 10:28:48 +0530

    updates

--- frontend/src/Components/10-12/RegisterForm.jsx
+++ frontend/src/Components/10-12/RegisterForm.jsx
@@ -1,18 +1,40 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const RegisterForm = () => {
+    const router = useNavigate();
 
-    const [userData, setUserData] = useState({ name: "", email: "", password: "" })
+    const [userData, setUserData] = useState({ name: "", email: "", password: "", confirmpassword: "" })
     console.log(userData, "userData")
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        alert("Registration Completed..")
-        setUserData({ name: "", email: "", password: "" })
+        if (userData.name && userData.email && userData.password && userData.confirmpassword) {
+            if (userData.password === userData.confirmpassword) {
+                try {
+                    // const response = await axios.post('http://localhost:8000/register', { userData })
+
+                    const response = { data: { success: true, messsage: "Registeration COMPpelted." } }
+
+                    if (response.data.success === true) {
+                        alert(response.data.messsage)
+                        setUserData({ name: "", email: "", password: "", confirmpassword: "" })
+                        router('/login-form')
+                    }
+                } catch (error) {
+                    console.log(error)
+                    alert(error.response.data.messsage)
+                }
+            } else {
+                alert("Password and Confirm Password not matched.")
+            }
+        } else {
+            alert("All fields are required.")
+        }
     }
 
     function handleChange(e) {
         // console.log(e.target.value, e.target.name)
-        setUserData({ ...userData, [e.target.name]: e.target.value });
+        setUserData({...userData, [e.target.name]: e.target.value });
     }
     return (
         <div>
@@ -24,6 +46,8 @@ const RegisterForm = () => {
                 <input type='email' required onChange={handleChange} name='email' value={userData.email} /><br />
                 <label>Password</label><br />
                 <input type='password' required onChange={handleChange} name='password' value={userData.password} /><br />
+                <label>Confirm Password</label><br />
+                <input type='password' required onChange={handleChange} name='confirmpassword' value={userData.confirmpassword} /><br />
                 <input type='submit' value="Register" />
             </form>
         </div>
